6186e380b652416b32e2cb835991d83b1d374f94
Nicholas Bonanato <nicholas.stefanbonanato@gmail.com>
2020-11-28 03:34:36 -0200

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/java/prova_poo/Disciplina.java
    	web/disciplinas.jsp
    	web/index.jsp
    	web/sobre.jsp

--- src/java/prova_poo/Disciplina.java
+++ src/java/prova_poo/Disciplina.java
@@ -1,5 +1,7 @@
 package prova_poo;
 import java.util.ArrayList;
+import web.DbListener;
+import java.sql.*;
                 
 public class Disciplina {
     
@@ -8,7 +10,111 @@ public class Disciplina {
     int ciclo;
     float nota;
     
-    ArrayList<Disciplina> disciplinas = new ArrayList<>();
+    public static ArrayList<Disciplina> getList() throws Exception {
+    ArrayList<Disciplina> list = new ArrayList<>();
+    
+           Connection con = null;
+           Statement stmt = null;
+           Exception methodEx = null;
+           ResultSet rs = null;
+           
+         try {
+                con = DbListener.getConection();
+                stmt = con.createStatement();
+                stmt.execute(Disciplina.getCreatStatement());
+                    rs = stmt.executeQuery("SELECT * FROM disciplinas");
+                    
+               while (rs.next()) {
+                   list.add (new Disciplina(rs.getString("nome"), rs.getString("ementa"), rs.getInt("ciclo")));   
+               }
+      
+        } catch (Exception ex) {
+                methodEx = ex;
+        } finally {
+        } try {
+            stmt.close();
+            con.close();
+        } catch (Exception e) {}
+        
+      if(methodEx != null) throw methodEx;
+      return list;
+           
+    }
+    
+    public static void insert(String nome, String ementa, int ciclo, float nota) {
+        Connection con = null;
+           PreparedStatement stmt = null;
+           Exception methodEx = null;
+           ResultSet rs = null;
+           
+             try {
+                con = DbListener.getConection();
+                stmt = con.prepareStatement("INSERT INTO disciplinas values (?,?,?,?)");
+                stmt.setString(1, "nome");
+                stmt.setString(2, "ementa");
+                stmt.setString(3, " " + ciclo);
+                stmt.setString(4, " " + nota);
+                stmt.execute();
+      
+        } catch (Exception ex) {
+                methodEx = ex;
+        } finally {
+        } try {
+            stmt.close();
+            con.close();
+        } catch (Exception e) { }
+
+    }
+
+     public static void update(String nomeAntigo, String nome, String ementa, int ciclo, float nota) {
+        Connection con = null;
+           PreparedStatement stmt = null;
+           Exception methodEx = null;
+           ResultSet rs = null;
+           
+             try {
+                con = DbListener.getConection();
+                stmt = con.prepareStatement("UPDATE disciplinas SET nome=?, ementa=?, ciclo=?, nota=? WHERE nome=?)");
+                stmt.setString(1, "nome");
+                stmt.setString(2, "ementa");
+                stmt.setString(3, " " + ciclo);
+                stmt.setString(4, " " + nota);
+                stmt.setString(5, " " + nomeAntigo);
+                stmt.execute();
+      
+        } catch (Exception ex) {
+                methodEx = ex;
+        } finally {
+        } try {
+            stmt.close();
+            con.close();
+        } catch (Exception e) { }
+
+    }  
+    
+         public static void delete(String nome) {
+        Connection con = null;
+           PreparedStatement stmt = null;
+           Exception methodEx = null;
+           ResultSet rs = null;
+           
+             try {
+                con = DbListener.getConection();
+                stmt = con.prepareStatement("DELETE FROM disciplinas WHERE nome=?");
+                stmt.setString(1, "nome");
+                stmt.execute();
+      
+        } catch (Exception ex) {
+                methodEx = ex;
+        } finally {
+        } try {
+            stmt.close();
+            con.close();
+        } catch (Exception e) { }
+
+    }  
+    
+    
     
     
     public Disciplina(String nome, String ementa, int ciclo) {
@@ -18,13 +124,7 @@ public class Disciplina {
        
     }
     
-    public void addDisciplina(Disciplina disciplina){
-        disciplinas.add(disciplina);
-    }
-    
-    public ArrayList<Disciplina> getList() {
-        return disciplinas;
-    }
+
     
     public String getNome() {
     return nome;
@@ -46,8 +146,17 @@ public class Disciplina {
         nota = value;
     }
     
-    public void setList(ArrayList<Disciplina> list){
-        disciplinas = list;
+    
+    public static String getCreatStatement() {
+        return "CREATE TABLE IF NOT EXISTS disciplinas ("
+                + "nome VARCHAR(50) UNIQUE NOT NULL,"
+                + "ementa VARCHAR(100) NOT NULL,"
+                + "ciclo int,"
+                + "nota float )";
+        
     }
+    
+    
+    
     
 }
